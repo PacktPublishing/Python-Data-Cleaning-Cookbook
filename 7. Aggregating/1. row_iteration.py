371721fc3fde1fdc92df9f0515e3d43581e27075
michaelbwalker <mike@alrb.org>
2020-11-17 00:57:08 -0500

    large upload
    large upload

---
+++ 7. Aggregating/1. row_iteration.py
@@ -0,0 +1,61 @@
+# import pandas and numpy, and load the covid data
+import pandas as pd
+import numpy as np
+pd.set_option('display.width', 200)
+pd.set_option('display.max_columns', 35)
+pd.set_option('display.max_rows', 200)
+pd.options.display.float_format = '{:,.2f}'.format
+coviddaily = pd.read_pickle("data/coviddaily720.pkl")
+ltbrazil = pd.read_pickle("data/ltbrazil.pkl")
+
+# sort the covid data by location and case date in ascending order
+coviddaily = coviddaily.sort_values(['location','casedate'])
+
+# iterate over rows with itertuples, append to list with each change of group
+prevloc = 'ZZZ'
+rowlist = []
+for row in coviddaily.itertuples():
+  if (prevloc!=row.location):
+    if (prevloc!='ZZZ'):
+      rowlist.append({'location':prevloc, 'casecnt':casecnt})
+    casecnt = 0
+    prevloc = row.location
+  casecnt += row.new_cases
+  
+rowlist.append({'location':prevloc, 'casecnt':casecnt})
+len(rowlist)
+rowlist[0:4]
+
+# create a dataframe from the rowlist
+covidtotals = pd.DataFrame(rowlist)
+covidtotals.head()
+
+# sort the land temperatures data and drop rows with missing values for temperature
+ltbrazil = ltbrazil.sort_values(['station','month'])
+ltbrazil = ltbrazil.dropna(subset=['temperature'])
+
+# iterate over rows with itertuples, append to list with each change of group
+prevstation = 'ZZZ'
+prevtemp = 0
+rowlist = []
+for row in ltbrazil.itertuples():
+  if (prevstation!=row.station):
+    if (prevstation!='ZZZ'):
+      rowlist.append({'station':prevstation, 'avgtemp':tempcnt/stationcnt, 'stationcnt':stationcnt})
+    tempcnt = 0
+    stationcnt = 0
+    prevstation = row.station
+
+  # choose only rows that are within 3 degrees of the previous temperature  
+  if ((0 <= abs(row.temperature-prevtemp) <= 3) or (stationcnt==0)):
+    tempcnt += row.temperature
+    stationcnt += 1
+  
+  prevtemp = row.temperature
+
+rowlist.append({'station':prevstation, 'avgtemp':tempcnt/stationcnt, 'stationcnt':stationcnt})
+rowlist[0:5]
+ltbrazilavgs = pd.DataFrame(rowlist)
+ltbrazilavgs.head()
+
+
---
+++ 7. Aggregating/4. groupby_more.py
@@ -0,0 +1,41 @@
+# import pandas, load the nls97 feather file
+import pandas as pd
+pd.set_option('display.width', 90)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_rows', 30)
+pd.options.display.float_format = '{:,.0f}'.format
+nls97 = pd.read_pickle("data/nls97.pkl")
+
+# review the structure of the nls97 data
+nls97.iloc[:,0:7].info()
+
+# look again at some of the data
+catvars = ['gender','maritalstatus','highestdegree']
+
+for col in catvars:
+  print(col, nls97[col].value_counts(sort=False), sep="\n\n", end="\n\n\n")
+
+
+# review some descriptive statistics
+contvars = ['satmath','satverbal','weeksworked06','gpaoverall',
+  'childathome']
+
+nls97[contvars].describe()
+
+# look at sat math scores by gender
+nls97.groupby('gender')['satmath'].mean()
+
+# look at sat math scores by gender and highest degree earned
+nls97.groupby(['gender','highestdegree'])['satmath'].mean()
+
+# look at sat math and verbal scores by gender and highest degree earned
+nls97.groupby(['gender','highestdegree'])[['satmath','satverbal']].mean()
+
+# add max and standard deviations
+nls97.groupby(['gender','highestdegree'])['gpaoverall'].agg(['count','mean','max','std'])
+
+# use a dictionary for more complicated aggregations
+pd.options.display.float_format = '{:,.1f}'.format
+aggdict = {'weeksworked06':['count', 'mean', 'max','std'], 'childathome':['count', 'mean', 'max', 'std']}
+nls97.groupby(['highestdegree']).agg(aggdict)
+nls97.groupby(['maritalstatus']).agg(aggdict)
